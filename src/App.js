d2fc7982a873073d98eae3d01d96f5bce746c42e
Fede Bramati <bramatif@hotmail.com>
2021-08-04 01:05:34 -0300

    First commit

--- src/App.js
+++ src/App.js
@@ -6,8 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>Vinyl Record Store</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Tu tienda de música amiga
         </p>
         <a
           className="App-link"
