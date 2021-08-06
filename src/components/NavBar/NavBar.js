05ef0347f47785ca3bbac153171ccf62dd168c99
Fede Bramati <bramatif@hotmail.com>
2021-08-06 14:46:01 -0300

    NavBar creada

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,37 @@
+import "./NavBar.scss"
+const NavBar = () => {
+    return(
+        <header>
+            <nav className="nav">
+                <div className="nav__head">
+                    <div className="nav__head__logo">
+                        {/* <img src="" alt="LOGO"/> */}
+                        <h2>Vinyl Record Store</h2>
+                    </div>
+                    <form className="nav__head__srchBar">
+                        <input type="seach" placeholder="Que andas buscando"/>
+                        <div className="nav__head__srchBar__btn">
+                        <button type="submit">Buscar</button>
+                        </div>
+                    </form>
+                </div>
+                <div className="nav__menu">
+                    <ul className="nav__menu__main">
+                        <li className="nav__menu__main__li"><a href="" className="nav__menu__main__li--borde">Inicio</a></li>
+                        <li className="nav__menu__main__li nav__menu__main__li--subDisp"><a href="">Vinilo</a>
+                            <ul className="nav__menu__main__submenu">
+                                <li><a href="">Rock/Pop</a></li>
+                                <li><a href="">Nacional</a></li>
+                                <li><a href="">Jazz</a></li>                            
+                            </ul>
+                        </li>
+                        <li className="nav__menu__main__li"><a href="">Boxsets</a></li>
+                        <li className="nav__menu__main__li"><a href="">Mi cuenta</a></li>                        
+                    </ul>
+                </div>            
+            </nav>
+        </header>
+    )
+}
+
+export default NavBar;
